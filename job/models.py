ac46ddc612b1920faf2d007462670fbc12250e39
J01 <Jolan133st@gmail.com>
2023-02-25 15:07:12 +0200

    add view

--- job/models.py
+++ job/models.py
@@ -6,6 +6,8 @@ JOB_TYPE = (
     ("Full Time","Full Time"),
     ("Part Time","Part Time"),
 )
+
+# create table in DataBase 
 class Job(models.Model):
     title = models.CharField(max_length=100) #columns
     #location = 
@@ -15,8 +17,14 @@ class Job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=0)
-     
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)  
+
     def __str__(self):
         return self.title
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name 
