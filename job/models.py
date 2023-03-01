6c909c773c156873655ea2e2c1610b5dc1eaab35
J01 <Jolan133st@gmail.com>
2023-03-01 22:04:06 +0200

    rest of it

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -24,10 +25,39 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=0)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)  
-    image = models.ImageField(upload_to=image_upload)  
+    image = models.ImageField(upload_to=image_upload)
+    slug = models.SlugField(blank=True , null=True)  
 
     def __str__(self):
         return self.title
+
+    def save(self,*args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job,self ).save( *args,**kwargs)
+
+
+class Apply(models.Model):
+    job = models.ForeignKey("Job", related_name= "apply_job" , on_delete=models.CASCADE )  
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    website = models.URLField()
+    cv  = models.FileField(upload_to="apply/")
+    cover_letter = models.TextField(max_length=500)
+    create  = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
 
 
 class Category(models.Model):
